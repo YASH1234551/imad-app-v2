3219aebad5557470f784f6898471fc688d36533b
YASH <yashmittal_de@srmuniv.edu.in>
2017-02-10 23:37:02 +0530

    Three articles added.

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res) {
   res.send('Article One is Requested and will be served');
 });
-
+app.get('/article-two', function (req, res) {
+  res.send('Article Two is Requested and will be served');
+});
+app.get('/article-three', function (req, res) {
+  res.send('Article Three is Requested and will be served');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

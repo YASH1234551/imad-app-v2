003ed230aca656288aa5cdae14cb7f1c42f0db72
YASH1234551 <yashmittal_de@srmuniv.edu.in>
2017-02-12 15:11:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,4 @@
-var img=document.getElementById('modi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=maginLeft + 10;
-    img.style.marginLeft= marginLeft +'px';
-}
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,10);
-}
+
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){

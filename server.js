dac4ead2ce7cec6b97f4562265418a26a9e0d237
YASH1234551 <yashmittal_de@srmuniv.edu.in>
2017-02-12 15:08:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,10 +91,10 @@ function createTemplate (data)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var count=0;
+var counter=0;
 app.get('/counter', function (req, res) {
-    count=count+1;
-    res.send(count.toString());
+    counter=counter+1;
+    res.send(counter.toString());
 });
 app.get('/:articleName', function (req, res) {
    var articleName=req.params.articleName;

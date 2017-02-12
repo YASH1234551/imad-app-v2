e1db92050990a4165be952fa690f3e796586ca3c
YASH1234551 <yashmittal_de@srmuniv.edu.in>
2017-02-12 14:54:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,11 @@ app.get('/:articleName', function (req, res) {
    var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
 });
+var count=0;
+app.get('/counter', function (req, res) {
+    count=count+1;
+    res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

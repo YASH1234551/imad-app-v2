c4db86ec017c263146aec4606117aac966992113
YASH1234551 <yashmittal_de@srmuniv.edu.in>
2017-02-12 13:35:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('Loaded!');
 var img=document.getElementById('modi');
+var moveLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+ 'px';
+}
 img.onclick= function()
 {
-    img.style.marginLeft='100px';
+    var internal=SetInternal(moveRight,100);
 };

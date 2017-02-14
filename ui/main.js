653404c29fd2fc4b31c7b0cb2758fb81ad00dfcb
YASH1234551 <yashmittal_de@srmuniv.edu.in>
2017-02-14 23:17:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,22 @@
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick = function(){
-  counter=counter +1;
-  var span=document.getElementById('count');
-  span.innerHTML =counter.toString();
+    var request=new XMLHttpRequest();
+    request.onreadystatechange =function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+               counter=counter +1;
+               var span=document.getElementById('count');
+               span.innerHTML =counter.toString();   
+            }
+        }
+    };
+    
+    request.open('GET','http://yash1234551.imad.hasura-app.io/counter');
+    request.send(null);
+    
+    
 };

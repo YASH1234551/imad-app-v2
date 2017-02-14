0f463e46f76e6a60fd1c887ea6088e533da5e608
YASH1234551 <yashmittal_de@srmuniv.edu.in>
2017-02-15 00:07:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,13 @@ app.get('/counter', function (req, res) {
     counter=counter+1;
     res.send(counter.toString());
 });
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+   var name=req.params.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
    var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -110,12 +117,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-var names=[];
-app.get('/submit-name/:name', function (req, res) {
-   var name=req.params.name;
-   names.push(name);
-   res.send(JSON.stringify(names));
-});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
